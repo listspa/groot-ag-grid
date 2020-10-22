f211b1547cce786dc5593b405a22c1a2050a6eff
Andrea Bergia <a.bergia@list-group.com>
2020-10-22 13:11:17 +0200

    fix: added missing files in `public-api`

--- projects/groot-ag-grid/src/public-api.ts
+++ projects/groot-ag-grid/src/public-api.ts
@@ -3,10 +3,6 @@
  */
 
 // Please keep this list in alphabetical order!!
-export * from './lib/groot-ag-grid/groot-ag-grid.component';
-export * from './lib/groot-ag-grid/groot-ag-grid-customization.consts';
-export * from './lib/groot-ag-grid/groot-ag-grid-customization.service';
-export * from './lib/groot-ag-grid/groot-ag-grid.module';
 export * from './lib/groot-ag-grid/groot-ag-grid-header-template/groot-ag-grid-header-template.component';
 export * from './lib/groot-ag-grid/groot-ag-grid-loading-overlay/groot-ag-grid-loading-overlay.component';
 export * from './lib/groot-ag-grid/groot-ag-grid-no-rows-overlay/groot-ag-grid-no-rows-overlay.component';
@@ -14,3 +10,9 @@ export * from './lib/groot-ag-grid/groot-ag-grid-renderer-booleans/groot-ag-grid
 export * from './lib/groot-ag-grid/groot-ag-grid-renderer-dates/groot-ag-grid-renderer-dates.component';
 export * from './lib/groot-ag-grid/groot-ag-grid-renderer-numbers/groot-ag-grid-renderer-numbers.component';
 export * from './lib/groot-ag-grid/groot-ag-grid-renderer-template/groot-ag-grid-renderer-template.component';
+export * from './lib/groot-ag-grid/groot-ag-grid.component';
+export * from './lib/groot-ag-grid/groot-ag-grid-customization.consts';
+export * from './lib/groot-ag-grid/groot-ag-grid.module';
+export * from './lib/groot-ag-grid/groot-ag-grid-customization.service';
+export * from './lib/groot-ag-grid/groot-ag-grid-selection.model';
+export * from './lib/groot-ag-grid/no-grid-data.model';
