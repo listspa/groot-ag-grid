f151598bb6ebee784f8e8e6a6c02ae39d914050d
Paolo Inaudi <p.inaudi@list-group.com>
2023-08-11 18:34:39 +0200

    feat: create interface MultiSortFilterPaginationOptions

--- projects/groot-ag-grid/src/lib/groot-ag-grid/groot-ag-grid-pagination.model.ts
+++ projects/groot-ag-grid/src/lib/groot-ag-grid/groot-ag-grid-pagination.model.ts
@@ -1,8 +1,12 @@
-import {PaginationOptions, SortPagination} from "@listgroup/groot";
+import {FilterOption, PaginationOptions, SortPagination} from "@listgroup/groot";
 
 export interface MultipleSortPagination {
   sort: SortPagination[]
 }
 
 export interface MultiSortPaginationOptions extends PaginationOptions, MultipleSortPagination {
+}
+
+export interface MultiSortFilterPaginationOptions extends MultiSortPaginationOptions {
+  filters: FilterOption[];
 }
