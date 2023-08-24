55615928a4507ad60ad3dac3a64450db30bc0724
Alessandro Versace <a.versace@list-group.com>
2023-08-24 09:57:06 +0200

    feat: added demo-style page to play with ul lists style

--- projects/demo-app/src/app/app-routing.module.ts
+++ projects/demo-app/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import {RouterModule, Routes} from '@angular/router';
 import {PageDemoTableComponent} from './pages/page-demo-table/page-demo-table.component';
 import {PageHomepageComponent} from './pages/page-homepage/page-homepage.component';
 import {PageCustomComponentsComponent} from './pages/page-custom-components/page-custom-components.component';
+import {PageDemoStyleComponent} from './pages/page-demo-style/page-demo-style.component';
 
 const routes: Routes = [
   {path: '', pathMatch: 'full', redirectTo: 'home'},
   {path: 'home', component: PageHomepageComponent},
   {path: 'demo-simple', component: PageDemoTableComponent},
   {path: 'custom-components', component: PageCustomComponentsComponent},
+  {path: 'demo-style', component: PageDemoStyleComponent},
 ];
 
 @NgModule({
