9f8c691c4674b29403940f12046f2b764a94bd00
Paolo Inaudi <p.inaudi@list-group.com>
2023-10-27 17:27:16 +0200

    feat: make formats easily customizable

--- projects/groot-ag-grid/src/lib/groot-ag-grid/groot-ag-grid-renderer-dates/groot-ag-grid-renderer-dates.component.ts
+++ projects/groot-ag-grid/src/lib/groot-ag-grid/groot-ag-grid-renderer-dates/groot-ag-grid-renderer-dates.component.ts
@@ -2,24 +2,38 @@ import {Component} from '@angular/core';
 import {ICellRendererAngularComp} from 'ag-grid-angular';
 import {BsDatepickerConfig} from 'ngx-bootstrap/datepicker';
 import {normalizeNgBootstrapDateFormat} from '@listgroup/groot';
+import {ICellRendererParams} from "ag-grid-community";
+
+interface RendererDatesParams extends ICellRendererParams {
+  timestamp?: boolean;
+  showMilliseconds?: boolean;
+  dateFormat?: string;
+  timeFormat?: string;
+  millisecondsFormat?: string;
+}
 
 @Component({
   templateUrl: './groot-ag-grid-renderer-date.component.html'
 })
 export class GrootAgGridRendererDatesComponent implements ICellRendererAngularComp {
-  params: any;
+  params: RendererDatesParams;
   dateFormat: string;
+  timeFormat: string;
+  millisecondsFormat: string;
 
   constructor(bsDatepickerConfig: BsDatepickerConfig) {
     this.dateFormat = normalizeNgBootstrapDateFormat(bsDatepickerConfig.dateInputFormat);
   }
 
-  agInit(params: any): void {
+  agInit(params: RendererDatesParams): void {
     this.params = params;
+    this.dateFormat = this.params?.dateFormat ?? this.dateFormat;
+    this.timeFormat = this.params?.timeFormat ?? 'HH:mm:ss';
+    this.millisecondsFormat = this.params?.millisecondsFormat ?? '.SSS';
   }
 
-  refresh(params: any): boolean {
-    this.params = params;
+  refresh(params: RendererDatesParams): boolean {
+    this.agInit(params);
     return true;
   }
 }
