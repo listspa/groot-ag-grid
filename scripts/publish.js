2e7d412ba14cd25ed01a55de8c25b0ff2e2415e9
Samuele Dallape <s.dallape@list-group.com>
2024-07-23 15:56:46 +0200

    fix: spawn on win failing errno: -4071,

--- scripts/publish.js
+++ scripts/publish.js
@@ -6,4 +6,4 @@ if (tgz.length !== 1) {
   throw new Error("Expected to find exactly one tgz file in dist");
 }
 
-spawn(/^win/.test(process.platform) ? 'npm.cmd' : 'npm', ['publish', tgz[0], '--verbose'], {stdio: 'inherit'});
+spawn(/^win/.test(process.platform) ? 'npm.cmd' : 'npm', ['publish', tgz[0], '--verbose'], {stdio: 'inherit', shell: true});
